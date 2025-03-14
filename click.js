305d54b7f4a4945ea512a175565d1cbe055c1df7
badri <badrinadh4641@gmail.com>
2025-03-14 17:03:57 +0530

    intial commit

---
+++ click.js
@@ -0,0 +1,22 @@
+let backgroundValue=document.getElementById("total-Background");
+let text=document.getElementById("bg-color");
+function brownClick(){
+    backgroundValue.style.backgroundColor="#e0e0e0";
+    text.textContent="#e0e0e0";
+    text.style.color="#e0e0e0";
+}
+function greenClick(){
+    backgroundValue.style.backgroundColor="#6fcf97";
+    text.textContent="#6fcf97";
+    text.style.color="#6fcf97";
+}
+function blueClick(){
+    backgroundValue.style.backgroundColor="#56ccf2";
+    text.textContent="#56ccf2";
+    text.style.color="#56ccf2";
+}
+function purpleClick(){
+    backgroundValue.style.backgroundColor="#bb6bd9";
+    text.textContent="#bb6bd9";
+    text.style.color="#bb6bd9";
+}
